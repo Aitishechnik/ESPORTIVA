e9c4d3ecc600005d4161f11e1bde1261c93c7f44
Aitishechnik <aitishechka666@gmail.com>
2023-11-30 17:33:10 +0300

    SetActiveRecursively added (shifting menues)

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private RectTransform _activeUI;
+
+    [SerializeField]
+    private Button _miniGameButton;
+
+    [SerializeField]
+    private Button _matchesButton;
+
+    [SerializeField]
+    private Button _settingsButton;
+
+    [SerializeField]
+    private Button _returnButton;
+
+    [SerializeField]
+    private MatchGameManager _gameManager;
+
+    [SerializeField]
+    private RectTransform _mainMenu;
+
+    [SerializeField]
+    private RectTransform _miniGame;
+
+    private void Awake()
     {
-        
+        _activeUI = _mainMenu;
+        _miniGameButton.onClick.AddListener(()=> StartMiniGame(_miniGame));
+        _returnButton.onClick.AddListener(() => StartMiniGame(_mainMenu));
+    }
+    private void StartMiniGame(RectTransform rect)
+    {
+        SetActiveRecursively(rect, true);
+        SetActiveRecursively(_activeUI, false);
+        _activeUI = rect;
     }
 
-    // Update is called once per frame
-    void Update()
+    private static void SetActiveRecursively(RectTransform obj, bool isActive)
     {
-        
+        obj.gameObject.SetActive(isActive);
+
+        for(int i = 0; i < obj.childCount; i++)
+        {
+            var child = obj.GetChild(i) as RectTransform;
+            SetActiveRecursively(child, isActive);
+        }
     }
 }
